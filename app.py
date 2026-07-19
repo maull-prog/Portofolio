abd80852a5e57b4b8a2aae76025e04a55f280ffa
Pradika maula Arrozaq <pradikamaula01@gmail.com>
2026-07-19 19:05:47 +0700

    update vercel

--- app.py
+++ app.py
@@ -126,6 +126,8 @@ def create_app():
             
     return app
 
+# Expose app for Vercel deployment
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
